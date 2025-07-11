253398469b1b58248d520a9fdc71dfa0f3016b02
Arthur Madureira <arthurrobinsonm@gmail.com>
2025-07-11 10:41:36 -0300

    Google Analytics

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -8,21 +8,23 @@ interface GoogleAnalyticsProps {
 }
 
 export function GoogleAnalytics({ gaId }: GoogleAnalyticsProps) {
+  console.log('Loading Google Analytics with ID:', gaId)
+  
   return (
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
         strategy="afterInteractive"
+        async
+        onLoad={() => console.log('Google Analytics script loaded')}
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${gaId}', {
-            page_title: document.title,
-            page_location: window.location.href,
-          });
+          gtag('config', '${gaId}');
+          console.log('Google Analytics configured with ID: ${gaId}');
         `}
       </Script>
     </>
